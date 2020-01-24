a3217cac4e25ae671e0a8129328599f271a2ca60
IAmJahoo <janhor.astro@gmail.com>
2020-01-24 16:44:49 +0100

    Add layout and header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>About me</h1>
     <p>That's my personal webpage</p>
     <Link to="/">&larr; back to homepage </Link>
-  </>
+  </Layout>
 );
