6c50287aa64c0c9c8dc437eb7fc7c7d82c1c5a42
IAmJahoo <janhor.astro@gmail.com>
2020-01-26 21:28:18 +0100

    loading mdx posts with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/layout';
 
-export default () => (
-  <Layout>
-    <h1>Home page</h1>
-    <p>Hello, frontend masters</p>
-    <Link to="/about">To about page</Link>
-  </Layout>
-);
+import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import usePosts from '../hooks/use-posts';
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home page</h1>
+      <p>Hello, frontend masters</p>
+      <Link to="/about">To about page</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
